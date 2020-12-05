6777dba4e6aede648093fef5d0379f8ef06c70c7
Impact-yxw <yixingwei4@gmail.com>
2020-12-05 15:25:11 +0800

    win git

--- CCF真题/2018/12/小明放学/test.cpp
+++ CCF真题/2018/12/小明放学/test.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
-int main()
-{
-    int r,y,g = 0;
-    std::cin >> r >> y >> g;
-    int n = 0;
-    std::cin >> n;
-    int k,t = 0;
-    int timeres = 0;//æœ€åç»“æœ
-    for (int i = 0;i < n;i++){
-        std::cin >> k >> t;
-        if(k == 0||k == 1){
-            timeres += t;
-        }else if(k == 2){
-            timeres += t+r;
-        }
-    }
-    std::cout << timeres << std::endl;
-    return 0;
+
+int main() {
+	int r, y, g = 0;
+	std::cin >> r >> y >> g;
+	int n = 0;
+	std::cin >> n;
+	int k, t = 0;
+	int timeres = 0;
+	for (int i = 0; i < n; i++) {//¿¼ÂÇµ½¿ª·¢ÁËµÄ¿§·Èµê
+		if (k == 0 || k == 1) {
+			timeres += t;
+		} else if (k == 2) {
+			timeres += t + r;
+		}
+	}
+	std::cout << timeres << std::endl;
+	return 0;
 }
