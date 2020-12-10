e2a956d8d4a95a4aa0dd27e847b9fc581338263d
Impact-yxw <yixingwei4@gmail.com>
2020-12-10 21:54:02 +0800

    nothong

--- CCF真题/2018/12/小明放学/test.cpp
+++ CCF真题/2018/12/小明放学/test.cpp
@@ -3,17 +3,29 @@
 int main() {
 	int r, y, g = 0;
 	std::cin >> r >> y >> g;
+    int count = r+y+g;
 	int n = 0;
 	std::cin >> n;
+
 	int k, t = 0;
-	int timeres = 0;
-	for (int i = 0; i < n; i++) {//考虑到开发了的咖啡店
-		if (k == 0 || k == 1) {
-			timeres += t;
-		} else if (k == 2) {
-			timeres += t + r;
-		}
+    long long timeres,temptime = 0;
+	for (int i = 0; i < n; i++) {
+        std::cin >> k >> t;
+        if(k == 1){
+            temptime = (timeres-t+count)%count;
+        }else if(k == 2){
+            temptime = (timeres-t-r+count)%count;
+        }else if (k == 3){
+            temptime = (timeres-t-y-r+count)%count;
+        }else{
+            timeres += t;
+            continue;
+        }
+        if(temptime < g){
+        }else{
+            timeres += count-temptime;
+        }
 	}
-	std::cout << timeres << std::endl;
+    std::cout << timeres << std::endl;
 	return 0;
 }
---
+++ CCF真题/2018/9/买菜/test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    int n = 0;
+    std::cin >> n;
+
+    int , b = 0;
+    for(int i = 0;i < n;i++){
+        
+    }
+
+    for (int i = n;i < 2*n;i++){
+
+    }
+    std::cout << "Hello world" << std::endl;
+    return 0;
+}
