43a507786aeba184a6b9ce11fec826acce174364
impact-eintr <yixingwei4@gmail.com>
2020-12-11 10:34:03 +0800

    nothing

--- CCF真题/2018/9/买菜/test.cpp
+++ CCF真题/2018/9/买菜/test.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
-
+#include <vector>
 int main()
 {
     int n = 0;
     std::cin >> n;
 
-    int , b = 0;
+    std::vector<int> alist;
+    std::vector<int> blist;
+    int a, b = 0;
     for(int i = 0;i < n;i++){
-        
+        std::cin >> a >> b;
+        alist.push_back(a);
+        alist.push_back(b);
     }
 
-    for (int i = n;i < 2*n;i++){
-
+    for (int i = 0;i < n;i++){
+        std::cin >> a >> b;
+        blist.push_back(a);
+        blist.push_back(b);
     }
-    std::cout << "Hello world" << std::endl;
+
+    int res = 0;
+    for (int i = 0;i < 2*n;i+=2){
+        a = alist[i]>blist[i]?alist[i]:blist[i];
+        b = alist[i+1]<blist[i+1]?alist[i+1]:blist[i+1];
+        res += b-a;
+    }
+    std::cout << res << std::endl;
     return 0;
 }
---
+++ CCF真题/2018/9/元素选择器/test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+struct node{
+    int data;
+    std::vector<int> child;
+} Node[100];
+
+int main()
+{
+    int n,m = 0;
+    std::cin >> n >> m;
+    
+    for (int i = 0;i < n;i++){
+
+    }
+    std::cout << "Hello world" << std::endl;
+    return 0;
+}
+
